3c506bb9788c70f7cc67f0aaa8033749c9cc0eb8
Nerdkz <viniciusmendonca@gec.inatel.br>
2019-05-24 00:32:46 -0300

    Atualização para nova entrega

--- src/main/java/com/mycompany/ec212/model/JCamera.java
+++ src/main/java/com/mycompany/ec212/model/JCamera.java
@@ -7,6 +7,7 @@ package com.mycompany.ec212.model;
 
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
+import org.lwjgl.util.vector.Matrix;
 
 /**
  *
@@ -31,14 +32,17 @@ public class JCamera {
     
     public void lookAt(Vector3 position, Vector3 target, Vector3 up){
         
-        Vector3 w = position.sub(target).nor();
+        Vector3 w = new Vector3(position).sub(target).nor();
         Vector3 u = up.crs(w).nor();
         Vector3 v = w.crs(u);
         
-        Matrix4 WorldMatrix4 = new Matrix4(u.x, v.x, w.x, position.x,
-                                          u.y, v.y, w.y, position.y,
-                                          u.z, v.z, w.z, position.z,
-                                          0, 0 ,0, 1);                                       
+        float values[] = new  float[]{u.x, v.x, w.x, position.x,
+                u.y, v.y, w.y, position.y,
+                u.z, v.z, w.z, position.z,                                       
+                0f, 0f ,0f, 1f};
+        
+        WorldMatrix = new Matrix4(values);
+        
     }
     
     public JRay generateRay(float x, float y, Vector3 sample) {
@@ -47,9 +51,11 @@ public class JCamera {
         float D = (float) Math.tan(FOV/2);
         float xscreen = (float) (2*xndc - 1) * film.aspectRatio() * D;
         float yscreen = (float) (1 - 2*yndc) * D;
+        
         Vector3 pixel = new Vector3(xscreen, yscreen, -1);
-        pixel = WorldMatrix.mul(pixel); //verificar isso aqui !!!!!
+        pixel = pixel.traMul(WorldMatrix); 
         Vector3 position = new Vector3(); //extrair a posição da camera de world matrix
+        position = WorldMatrix.getTranslation(position);
         Vector3 direction = pixel.sub(position).nor();//criar direção do raio
         return new JRay(position, direction);
     }
